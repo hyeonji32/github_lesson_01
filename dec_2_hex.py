44cd71bbb2c71377f5cf93a945f98db66f5f217a
hyeonji32 <131431174+hyeonji32@users.noreply.github.com>
2023-04-27 14:54:18 +0900

    Update dec_2_hex.py

--- dec_2_hex.py
+++ dec_2_hex.py
@@ -2,9 +2,7 @@ def decimal_to_hexadecimal(decimal_num):
     """
     Convert decimal number to hexadecimal.
     """
-    if decimal_num == 0:
-        return "0"
-    hex_chars = "0123456789ABCDEF"
+ 
     hexadecimal = ""
     while decimal_num > 0:
         remainder = decimal_num % 16
